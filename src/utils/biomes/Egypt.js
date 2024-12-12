3964971cd6e9c290c1e3ef8ca1f9ef7ee3df789e
Umesh KC <ufocalypse@gmail.com>
2024-12-12 21:56:05 +0545

    Fine tune egypt graph

--- src/utils/biomes/Egypt.js
+++ src/utils/biomes/Egypt.js
@@ -10,6 +10,7 @@ class Egypt extends BiomeGen {
         graph.addNodes([
             "eg_art",
             "eg_cacti",
+            "eg_cacti2",
             "eg_plr",
             "eg_puma",
             "eg_pyramid",
@@ -17,10 +18,9 @@ class Egypt extends BiomeGen {
             "eg_shrub",
             "eg_shrub_flipped",
             "bush",
-            "eg_statue",
-            "eg_tomb",
-            "eg_tomb_flipped",
+            "eg_statue_flipped",
             "eg_vsl",
+            "palm0",
             "palm1",
             "eg_gate1",
             "eg_gate2",
@@ -29,42 +29,49 @@ class Egypt extends BiomeGen {
         ])
         graph.setInitialNode("eg_gate2")
         graph.chain(["eg_gate2", "eg_gate1", "eg_gate1_flipped", "eg_gate2_flipped"])
-        graph.addEdge("eg_gate2_flipped", "eg_cacti", 50)
-        graph.addEdge("eg_cacti", "eg_pyramid", 48)
-        graph.addEdge("eg_cacti", "eg_cacti", 24, 2)
-        graph.addEdge("eg_cacti", "eg_vsl", 32)
-
         graph.addEdge("eg_pyramid", "eg_pyramid_flipped", 0)
-        graph.addEdge("eg_pyramid_flipped", "eg_pyramid", 48)
-        graph.addEdge("eg_pyramid_flipped", "eg_shrub", 48)
-
         graph.addEdge("eg_shrub", "eg_shrub_flipped", 0)
-        graph.addEdge("eg_shrub_flipped", "eg_shrub", 16)
-        graph.addEdge("eg_shrub_flipped", "eg_pyramid", 48)
-        graph.addEdge("eg_shrub_flipped", "eg_vsl", 32)
-        graph.addEdge("eg_shrub_flipped", "eg_art", 32)
-        graph.addEdge("eg_shrub_flipped", "palm1", 32, 3)
 
-        graph.addEdge("eg_vsl", "eg_vsl", 12, 1.5)
-        graph.addEdge("eg_vsl", "eg_art", 36)
-        graph.addEdge("eg_art", "eg_shrub", 36)
-        graph.addEdge("eg_art", "palm1", 36)
+        graph.addEdge("eg_pyramid_flipped", "palm0", 24)
+        graph.addEdge("eg_pyramid_flipped", "bush", 24, 1.5)
+        graph.addEdge("eg_pyramid_flipped", "eg_shrub", 24, 1.5)
+        graph.addEdge("eg_pyramid_flipped", "palm1", [24, 56])
+        graph.addEdge("eg_pyramid_flipped", "eg_pyramid", [-40, 20])
 
-        graph.addEdge("palm1", "palm1", 26, 2)
-        graph.addEdge("palm1", "eg_plr", 26)
+        graph.addEdge("eg_shrub_flipped", "eg_vsl")
+        graph.addEdge("eg_vsl", "eg_pyramid", 14)
+        graph.addEdge("eg_vsl", "eg_vsl", 10)
 
-        graph.addEdge("eg_plr", "eg_plr", 32, 1.5)
-        graph.addEdge("eg_plr", "eg_puma", 32)
+        graph.addEdge("palm0", "palm0", [-25, 12], 0.5)
+        graph.addEdge("palm0", "palm1", [-25, 12], 0.5)
+        graph.addEdge("palm1", "palm0", [-25, 12], 0.5)
+        graph.addEdge("palm1", "eg_plr", 16)
+        graph.addEdge("palm0", "eg_puma", [-12, 0])
+        graph.addEdge("palm1", "eg_puma", [-20, 0])
 
-        graph.addEdge("eg_puma", "bush", 32)
-        graph.addEdge("bush", "bush", 4, 2)
-        graph.addEdge("bush", "eg_statue", 8, 2)
-        graph.addEdge("bush", "eg_tomb", 12)
+        graph.addEdge("eg_plr", "eg_plr", 10, 0.5)
+        graph.addEdge("eg_plr", "bush", [40, 60])
+
+        graph.addEdge("bush", "eg_statue_flipped", 15)
+        graph.addEdge("bush", "eg_vsl", 15)
+        graph.addEdge("bush", "bush", [-10, -4], 0.5)
+        graph.addEdge("bush", "eg_cacti", [30, 40], 0.5)
+        graph.addEdge("bush", "eg_pyramid", [12, 24], 0.5)
+        graph.addEdge("eg_statue_flipped", "eg_statue_flipped", 10, 0.6)
+        graph.addEdge("eg_statue_flipped", "eg_art", 40, 0.6)
+        graph.addEdge("eg_statue_flipped", "eg_pyramid", 40)
+
+
+        graph.addEdge("eg_puma", "eg_cacti", 28)
+        graph.addEdge("eg_cacti", "palm0", 40)
+        graph.addEdge("eg_pyramid_flipped", "eg_cacti", 17)
+        graph.addEdge("eg_cacti", "eg_cacti2", 5, 0.5)
+        graph.addEdge("eg_cacti2", "eg_cacti", [26, 40], 0.5)
+        graph.addEdge("eg_cacti2", "eg_gate2", 50)
         
-        graph.addEdge("eg_statue", "eg_statue", 20)
-        graph.addEdge("eg_statue", "bush", 32)
+        graph.addEdge("eg_gate2_flipped", "bush", [30, 50])
 
-        graph.addEdge("eg_tomb", "eg_tomb_flipped")
+        graph.addEdge("eg_puma", "eg_art", 30)
     }
 }
 
