063e7f3bc401a7402b63413d27482a19be18d440
Umesh KC <ufocalypse@gmail.com>
2024-12-14 08:00:42 +0545

    Add watch tower subtexture

--- src/utils/BGen.js
+++ src/utils/BGen.js
@@ -4,11 +4,12 @@ import Construction from "./biomes/Construction"
 import Exotica from "./biomes/Exotica"
 import Japan from "./biomes/Japan"
 import Industrial from "./biomes/Industrial"
+import Medieval from "./biomes/Medieval"
 
 class BGen extends BGenMachine {
     constructor(atlasmeta, baseWidth=48, baseHeight=64) {
-        super([ Egypt, Construction, Exotica, Japan, Industrial ], atlasmeta, baseWidth, baseHeight)
-        this.setInitialBiome(Industrial)
+        super([ Egypt, Construction, Exotica, Japan, Industrial, Medieval ], atlasmeta, baseWidth, baseHeight)
+        this.setInitialBiome(Medieval)
     }
 }
 
---
+++ src/utils/biomes/Medieval.js
@@ -0,0 +1,32 @@
+import {  BiomeGen } from "@lib/utils/index.js"
+
+class Medieval extends BiomeGen {
+    height=4
+    constructor(stateMachine, dimsMap) {
+        super(stateMachine, dimsMap);
+        // TODO add transitions
+        this.addTransition("Industrial", 1)
+        const { objGraph: graph } = this
+        graph.addNodes([
+            "arch1",
+            "arch2",
+            "arch3",
+            "arch_5",
+            "btlmnt",
+            "base",
+            "base_dupe1",
+            "sb_haus2",
+            "watch_twr",
+        ])
+        graph.setInitialNode("btlmnt")
+        graph.addEdge("btlmnt", "arch2", -15)
+        graph.addEdge("arch2", "arch2", 0, 5)
+        graph.addEdge("arch2", "base", -15)
+        graph.addEdge("base", "arch2")
+        graph.addEdge("arch2", "arch2")
+        graph.addEdge("arch2", "sb_haus2", -6)
+
+    }
+}
+
+export default Medieval
