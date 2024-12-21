14aa9033939547add89daeaa5665d75926f9551f
Umesh KC <ufocalypse@gmail.com>
2024-12-21 14:04:02 +0545

    Sync the bones position with walk cycle

--- src/entities/Monster/index.js
+++ src/entities/Monster/index.js
@@ -2,10 +2,16 @@ import BoneAnimNode from "@lib/entities/BoneAnimNode";
 import animData from "./anim.json"
 
 class Monster extends BoneAnimNode {
+  noOverlay = true
   constructor(x, y, player) {
     super({ data: animData, pos: { x, y } });
     this.play("idle", "root state");
-    // this.syncroNode.scale = { x: -1, y: 1 }
+    this.scale = { x: -1, y: 1 }
+    this.play("run2", "run1")
+  }
+  update(dt, t) {
+    super.update(dt, t)
+    this.pos.x += -20 * dt
   }
 }
 
