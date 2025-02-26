5c181cf41bb020c3ec6d5dd3e450ec7c1bc81f6f
Umesh KC <ufocalypse@gmail.com>
2025-02-26 16:02:09 +0545

    get the collision with the monster perfect

--- src/entities/Monster/index.js
+++ src/entities/Monster/index.js
@@ -1,6 +1,9 @@
 import BoneAnimNode from "@lib/entities/BoneAnimNode";
 import { stateMachineMixin } from "@lib/utils/FSM";
 import animData from "./anim.json"
+import { getGlobalPos } from "@lib/utils/entity";
+import { TexRegion } from "@lib/index";
+import getTestFn from "@lib/components/Collision/helpers/getTestFn";
 
 // Define state classes
 class RunRightState {
@@ -96,6 +99,7 @@ class IdleLeftState {
 // Update Monster class to use external state classes
 class Monster extends BoneAnimNode {
   noOverlay = true
+
   constructor({ x, y, player, span = 200 }) {
     super({ data: animData, pos: { x: x, y } });
     this.player = player
@@ -113,16 +117,23 @@ class Monster extends BoneAnimNode {
     
     // Apply the state machine mixin
     stateMachineMixin(this, states);
-    
-    // Start with running right
     this.switchState('runRight');
+    this.syncroNode.hitCirc= {
+      x: -60, y: 0, radius: 80
+    }
+    this.testCol = getTestFn(this.syncroNode, this.player)
   }
   
   update(dt, t) {
     // Update the current state
     this.getState()?.update(dt);
-    
     // Call the parent update method
+    // this.player.pos.x = Math.round(getGlobalPos(this.syncroNode).x)
+    // Object.assign(this.player.pos, getGlobalPos(this.syncroNode))
+    if (this.testCol(this.syncroNode, this.player)) {
+      alert("HIT")
+    }
+    // this.noOverlay = Math.random() < 0.5
     super.update(dt, t);
   }
 }
