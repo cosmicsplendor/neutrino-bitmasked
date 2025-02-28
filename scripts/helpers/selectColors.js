e192e7504af9275c795b147974f86eccfb9daadb
Umesh KC <ufocalypse@gmail.com>
2025-02-28 16:34:54 +0545

    Update the bg and parallax bg colors to be lighter

--- scripts/helpers/selectColors.js
+++ scripts/helpers/selectColors.js
@@ -1,28 +1,43 @@
 const { pickOne, rand } = require("../utils")
 
-const originalColors = [
-    { "bg": "#112237", "pxbg": "#08111b", "tint": "0.01, -0.01, -0.005, 0" },
-    { "bg": "#121228", "pxbg": "#0f0f22", "tint": "0.025, -0.025, -0.025, 0" },
-    { "bg": "#121228", "pxbg": "#0f0f22", "tint": "0.025, -0.025, -0.025, 0" },
-    { "bg": "#222235", "pxbg": "#151525", "tint": "0.065, -0.025, 0.000, 0" },
-    { "bg": "#050525", "pxbg": "#000012", "tint": "0.075, -0.025, -0.0125, 0" },
-    { "bg": "#0f0f22", "pxbg": "#0a0a17", "tint": "0.025, -0.0125, -0.025, 0" },
-    { "bg": "#10103a", "pxbg": "#0b0b25", "tint": "0.05,0,-0.05,0" },
-    { "bg": "#132b27", "pxbg": "#0a1614", "tint": "0.025, -0.025, -0.0125, 0" },
-    { "pxbg": "#121212", "bg": "#171717", "tint": "0.025, 0.0125, -0.025, 0" }
+// Original colors for reference (commented out)
+// const originalColors = [
+//     { "bg": "#112237", "pxbg": "#08111b", "tint": "0.01, -0.01, -0.005, 0" },
+//     { "bg": "#121228", "pxbg": "#0f0f22", "tint": "0.025, -0.025, -0.025, 0" },
+//     { "bg": "#121228", "pxbg": "#0f0f22", "tint": "0.025, -0.025, -0.025, 0" },
+//     { "bg": "#222235", "pxbg": "#151525", "tint": "0.065, -0.025, 0.000, 0" },
+//     { "bg": "#050525", "pxbg": "#000012", "tint": "0.075, -0.025, -0.0125, 0" },
+//     { "bg": "#0f0f22", "pxbg": "#0a0a17", "tint": "0.025, -0.0125, -0.025, 0" },
+//     { "bg": "#10103a", "pxbg": "#0b0b25", "tint": "0.05,0,-0.05,0" },
+//     { "bg": "#132b27", "pxbg": "#0a1614", "tint": "0.025, -0.025, -0.0125, 0" },
+//     { "pxbg": "#121212", "bg": "#171717", "tint": "0.025, 0.0125, -0.025, 0" }
+// ]
+
+// Balanced colors - darker than previous suggestion but lighter than originals
+const balancedOriginalColors = [
+    { "bg": "#2a4670", "pxbg": "#182840", "tint": "0.01, -0.01, -0.005, 0" },
+    { "bg": "#2a2a55", "pxbg": "#1d1d40", "tint": "0.025, -0.025, -0.025, 0" },
+    { "bg": "#2a2a55", "pxbg": "#1d1d40", "tint": "0.025, -0.025, -0.025, 0" },
+    { "bg": "#3d3d60", "pxbg": "#282845", "tint": "0.065, -0.025, 0.000, 0" },
+    { "bg": "#22224f", "pxbg": "#15153d", "tint": "0.075, -0.025, -0.0125, 0" },
+    { "bg": "#27274a", "pxbg": "#1a1a35", "tint": "0.025, -0.0125, -0.025, 0" },
+    { "bg": "#2a2a75", "pxbg": "#1d1d55", "tint": "0.05,0,-0.05,0" },
+    { "bg": "#295652", "pxbg": "#1c3d3a", "tint": "0.025, -0.025, -0.0125, 0" },
+    { "bg": "#3a3a3a", "pxbg": "#252525", "tint": "0.025, 0.0125, -0.025, 0" }
 ]
 
-const aiSuggested = [
-    { "bg": "#1a1a30", "pxbg": "#0e0e24", "tint": "0.035, -0.02, -0.015, 0" }, // Deep blue-black
-    { "bg": "#16162e", "pxbg": "#121227", "tint": "0.05, -0.03, -0.01, 0" },    // Midnight blue
-    { "bg": "#1e2427", "pxbg": "#0c1014", "tint": "0.03, -0.02, -0.01, 0" },    // Charcoal grey-blue
-    { "bg": "#101822", "pxbg": "#0c121a", "tint": "0.03, 0, -0.03, 0" },        // Smokey blue-grey
-    { "bg": "#0b1a1e", "pxbg": "#030d12", "tint": "0.025, -0.02, -0.02, 0" },   // Cool dark cyan -corected
-    { "bg": "#20202d", "pxbg": "#10101d", "tint": "0.04, -0.03, -0.02, 0" },     // Shadowed navy - level16
-    { "bg": "#202033", "pxbg": "#181828", "tint": "0.04, -0.02, -0.015, 0" },
-    { "bg": "#1c1c40", "pxbg": "#101029", "tint": "0.06, -0.01, -0.03, 0" },
-    { "bg": "#162323", "pxbg": "#0a1414", "tint": "0.045, -0.02, -0.01, 0" },
-    { "bg": "#232323", "pxbg": "#121212", "tint": "0.03, -0.01, -0.01, 0" }
+// AI suggested colors with balanced adjustments
+const balancedAiSuggested = [
+    { "bg": "#3a3a70", "pxbg": "#2a2a55", "tint": "0.035, -0.02, -0.015, 0" }, // Deep blue
+    { "bg": "#35356c", "pxbg": "#252550", "tint": "0.05, -0.03, -0.01, 0" },    // Midnight blue
+    { "bg": "#3d4550", "pxbg": "#2a3240", "tint": "0.03, -0.02, -0.01, 0" },    // Charcoal grey-blue
+    { "bg": "#304055", "pxbg": "#203040", "tint": "0.03, 0, -0.03, 0" },        // Smokey blue-grey
+    { "bg": "#274a55", "pxbg": "#183540", "tint": "0.025, -0.02, -0.02, 0" },   // Cool cyan
+    { "bg": "#3f3f65", "pxbg": "#2d2d50", "tint": "0.04, -0.03, -0.02, 0" },    // Shadowed navy
+    { "bg": "#3a3a70", "pxbg": "#2d2d58", "tint": "0.04, -0.02, -0.015, 0" },
+    { "bg": "#35357f", "pxbg": "#282865", "tint": "0.06, -0.01, -0.03, 0" },
+    { "bg": "#345555", "pxbg": "#254040", "tint": "0.045, -0.02, -0.01, 0" },
+    { "bg": "#474747", "pxbg": "#353535", "tint": "0.03, -0.01, -0.01, 0" }
 ]
 
 const getMoon = () => {
@@ -38,9 +53,9 @@ const getMoon = () => {
 const selectColors = () => {
     const handcrafted = Math.random() < 0.65
     if (handcrafted) {
-        return pickOne(originalColors)
+        return pickOne(balancedOriginalColors)
     }
-    const colors = pickOne(aiSuggested)
+    const colors = pickOne(balancedAiSuggested)
     const moon = getMoon()
     return { ...colors, ...moon }
 }
