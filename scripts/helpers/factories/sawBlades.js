968551b12c6e35ceaddd58e87eb3d30e84639546
Umesh KC <ufocalypse@gmail.com>
2025-03-08 20:42:34 +0545

    Take into account dx and dy while calculating saw blades' position

--- scripts/helpers/factories/sawBlades.js
+++ scripts/helpers/factories/sawBlades.js
@@ -7,11 +7,11 @@ const sawBlades = (nameMap, static=false) => {
             return { width, height }
         },
         create: (params) => {
-            const { x, y, toX, toY, speed, size } = params
+            const { x, y, toX, toY, speed, size, dx, dy } = params
             if (static) return { x, y, name: nameMap[size] }
             return {
                 // these should come in relative grid space
-                x, y,
+                x: x  + +dx * TILE_SIZE, y: y + +dy * TILE_SIZE,
                 toX: x + Number(toX) * TILE_SIZE,
                 toY: y + Number(toY) * TILE_SIZE,
                 name: nameMap[size],
